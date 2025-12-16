e7822b5043902df7d2dc4350057b63703e879f4c
ZiadMostafaGit <mygithub2003@gmail.com>
2025-12-16 11:38:15 +0200

    update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -18,13 +18,86 @@ resource "aws_vpc" "k8s_cluster"{
 		Name="k8s_cluster_vpc"
 	}
 }
+
+///////////////////////////////////////////////////////////////////
+
+resource "aws_subnet" "private_subnet" {
+	vpc_id = aws_vpc.k8s_cluster.id
+	cidr_block = "10.0.1.0/24"
+	tags = {
+	  Name = "k8s_private_subnet"
+	}
+	}
+
+  
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+resource "aws_eip" "eip" {
+	domain = "vpc"
+  
+}
+
+
+resource "aws_nat_gateway" "k8s_nat_getway" {
+	allocation_id = aws_eip.eip.id
+	subnet_id = aws_subnet.public_subnet.id
+	tags = {
+	  Name = "k8s_nat_getway"
+	}
+
+	depends_on = [ aws_internet_gateway.k8s_internet_gateway ]
+	
+	}
+  
+
+resource "aws_route_table" "private_k8s_rout_table" {
+
+vpc_id = aws_vpc.k8s_cluster.id
+
+
+route {
+	cidr_block = "0.0.0.0/0"
+	nat_gateway_id = aws_nat_gateway.k8s_nat_getway.id
+}
+
+tags = {
+  Name = "private_k8s_rout_table"
+}
+
+
+
+}
+
+
+
+
+resource "aws_route_table_association" "private_rout_table_association" {
+	subnet_id = aws_subnet.private_subnet.id
+	route_table_id = aws_route_table.private_k8s_rout_table.id
+  
+}
+
+
+  
+
+
+
+
 
 resource "aws_subnet" "public_subnet"{
 
 
 vpc_id = aws_vpc.k8s_cluster.id
-cidr_block = "10.0.1.0/24"
+cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true   # IMPORTANT
 
 tags = {
@@ -56,7 +129,7 @@ resource "aws_internet_gateway" "k8s_internet_gateway"{
 
 //////////////////////////////////////////////////////////////////
 
-resource "aws_route_table" "k8s_route_table"{
+resource "aws_route_table" "public_k8s_route_table"{
 	vpc_id = aws_vpc.k8s_cluster.id
 	route{
 		cidr_block = "0.0.0.0/0"
@@ -64,14 +137,14 @@ resource "aws_route_table" "k8s_route_table"{
 	}
 
 	tags={
-		Name="k8s_rout_table"
+		Name="public_k8s_rout_table"
 	}
 }
 
 //////////////////////////////////////////////////////////////////////////////////
 resource "aws_route_table_association" "connect_k8s_subnet_with_route_table" {
 	subnet_id = aws_subnet.public_subnet.id
-	route_table_id = aws_route_table.k8s_route_table.id
+	route_table_id = aws_route_table.public_k8s_route_table.id
   
 }
 
